2e06085ddaff80abc075b57e678fe2acd7742a76
Diego Peixoto <111437909+DiegoPxt@users.noreply.github.com>
2025-02-24 21:24:43 -0300

    Delete Calculadora IMC.py

--- Calculadora IMC.py
+++
@@ -1,9 +0,0 @@
-print('----CALCULADORA DE IMC----')
-
-p = float(input('Digite seu peso: '))
-h = float(input('Digite sua altura: '))
-
-hAoQuadrado = h*h
-calculaImc = p/hAoQuadrado
-
-print('Seu IMC é: ', calculaImc)
